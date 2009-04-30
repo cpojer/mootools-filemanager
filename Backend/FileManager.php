664adeedd1e913f31a044261d85b01e857d4992d
Christoph Pojer <christoph.pojer@gmail.com>
2009-04-30 18:22:24 +0200

    * Fixing header(), closes #2

--- Backend/FileManager.php
+++ Backend/FileManager.php
@@ -60,7 +60,7 @@ class FileManager {
 			'Expires' => 'Fri, 01 Jan 1990 00:00:00 GMT',
 			'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate',
 		) as $key => $value)
-			header($key, $value);
+			header($key.': '.$value);
 		
 		$this->get = $_GET;
 		$this->post = $_POST;
