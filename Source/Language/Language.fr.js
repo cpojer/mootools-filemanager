8e40120de24bc30da80a4042323741738350c236
Christoph Pojer <christoph.pojer@gmail.com>
2009-05-06 11:10:48 +0200

    * Adding Finnish, Swedish, Spanish and French localization. Thank you everyone!

---
+++ Source/Language/Language.fr.js
@@ -0,0 +1,68 @@
+/*
+Script: Language.fr.js
+	MooTools FileManager - Language Strings in French
+
+Translation:
+	[Samuel Sanchez](http://www.kromack.com)
+*/
+
+FileManager.Language.fr = {
+	more: 'Détails',
+	width: 'Largeur :',
+	height: 'Hauteur :',
+	
+	ok: 'Ok',
+	open: 'Sélectionner un fichier',
+	upload: 'Téléverser',
+	create: 'Créer un dossier',
+	createdir: 'Merci de spécifier un nom de dossier :',
+	cancel: 'Annuler',
+	
+	information: 'Informations',
+	type: 'Type :',
+	size: 'Taille :',
+	dir: 'Chemin :',
+	modified: 'Modifié le :',
+	preview: 'Aperçu',
+	close: 'Fermer',
+	destroy: 'Supprimer',
+	destroyfile: 'Voulez-vous vraiment supprimer ce fichier ?',
+	
+	rename: 'Renommer',
+	renamefile: 'Merci de spécifier un nouveau nom de fichier :',
+	
+	download: 'Télécharger',
+	nopreview: '<i>Aucun aperçu disponible</i>',
+	
+	title: 'Titre :',
+	artist: 'Artiste :',
+	album: 'Album :',
+	length: 'Durée :',
+	bitrate: 'Débit :',
+	
+	deselect: 'Désélectionner',
+	
+	nodestroy: 'La suppression de fichier a été désactivée sur ce serveur.',
+	
+	'upload.disabled': 'Le téléversement de fichier a été désactivé sur ce serveur.',
+	'upload.authenticated': 'Vous n\'êtes pas authentifié et ne pouvez pas téléverser de fichier.',
+	'upload.path': 'Le répertoire de téléversement spécifié n\'existe pas. Merci de contacter l\'administrateur de ce site Internet.',
+	'upload.exists': 'Le chemin de téléversement spécifié existe déjà. Merci de contacter l\'administrateur de ce site Internet.',
+	'upload.mime': 'Le type de fichier spécifié n\'est pas autorisé.',
+	'upload.extension': 'Le fichier téléversé a une extension inconnue ou interdite.',
+	'upload.size': 'La taille de votre fichier est trop grande pour être téléversée sur ce serveur. Merci de sélectionner un fichier moins lourd.',
+	'upload.partial': 'Le fichier a été partiellement téléversé, merci de recommencer l\'opération.',
+	'upload.nofile': 'Aucun fichier n\'a été spécifié.',
+	'upload.default': 'Une erreur s\'est produite.',
+	
+	/* FU */
+	uploader: {
+		unknown: 'Erreur inconnue',
+		duplicate: 'Vous ne pouvez pas ajouter "<em>${name}</em>" (${size}), car l\'élément est déjà ajoutée !',
+		sizeLimitMin: 'Vous ne pouvez pas ajouter "<em>${name}</em>" (${size}), la taille minimale des fichiers est de <strong>${size_min}</strong>!',
+		sizeLimitMax: 'Vous ne pouvez pas ajouter "<em>${name}</em>" (${size}), la taille maximale des fichiers est de <strong>${size_max}</strong>!'
+	},
+	
+	resizeImages: 'Redimensionner les images pendant le téléversement',
+	flash: 'Dans le but de téléverser des fichiers, vous devez installer <a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash">Adobe Flash</a>.'
+};
