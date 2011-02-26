1599479f6b0003467646ebc9a84ceb34d6f529ab
Christoph Pojer <christoph.pojer@gmail.com>
2011-02-26 13:24:16 +0100

    Updating authors in the source

--- Source/FileManager.js
+++ Source/FileManager.js
@@ -3,13 +3,15 @@
 
 description: FileManager
 
+authors: Christoph Pojer (@cpojer), Fabian Vogelsteller (@frozeman)
+
+license: MIT-style license
+
 requires:
   core/1.3.1: '*'
   more/1.3.1.1: [Array.Extras, String.QueryString, Hash, Element.Delegation, Element.Measure, Fx.Scroll, Fx.SmoothScroll, Drag, Drag.Move, Assets, Tips ]
 
 provides: Filemanager
-
-license: MIT-style license
 
 ...
 */
