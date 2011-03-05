c302d48207f947389fb7476fb8e0c7f627a65a9d
Ger Hobbelt <ger@hobbelt.com>
2011-03-05 19:01:16 +0100

    a visit from the whitespace police -- just so the next commit's diff won't be fugly

--- Assets/Connector/Image.class.php
+++ Assets/Connector/Image.class.php
@@ -5,16 +5,18 @@
  *
  * @license MIT-style License
  * @author Christoph Pojer <christoph.pojer@gmail.com>
- * @author Additions: Fabian Vogelsteller <fabian.vogelsteller@gmail.com> 
+ * @author Additions: Fabian Vogelsteller <fabian.vogelsteller@gmail.com>
  *
  * @link http://www.bin-co.com/php/scripts/classes/gd_image/ Based on work by "Binny V A"
- * 
+ *
  * @version 1.11
  * Changlog<br>
- *    - 1.11 fixed $ratio in resize when both values are given 
+ *    - 1.11 fixed $ratio in resize when both values are given
  *    - 1.1 add real resizing, with comparison of ratio
  *    - 1.01 small fixes in process method and add set memory limit to a higher value
  */
+
+
 
 class Image {
 	/**
@@ -35,7 +37,7 @@ class Image {
 	 * @var array
 	 */
 	private $meta;
-	
+
 	/**
 	 * @param string $file The path to the image file
 	 */
@@ -68,7 +70,7 @@ class Image {
 			'mime' => $img['mime'],
 			'ext' => $ext_from_mime,
 		);
-		
+
 		if($this->meta['ext']=='jpg')
 			$this->meta['ext'] = 'jpeg';
 		if(!in_array($this->meta['ext'], array('gif', 'png', 'jpeg')))
@@ -76,7 +78,7 @@ class Image {
 		
 		if(in_array($this->meta['ext'], array('gif', 'png'))){
 			$this->image = $this->create();
-			
+
 			$fn = 'imagecreatefrom'.$this->meta['ext'];
 			$original = $fn($file);
 			imagecopyresampled($this->image, $original, 0, 0, 0, 0, $this->meta['width'], $this->meta['height'], $this->meta['width'], $this->meta['height']);
@@ -84,8 +86,8 @@ class Image {
 			$this->image = imagecreatefromjpeg($file);
 		}
 	}
-	
-	public function __destruct(){	  
+
+	public function __destruct(){
 		if(!empty($this->image)) imagedestroy($this->image);
 		unset($this->image);
 	}
@@ -101,7 +103,7 @@ class Image {
 			'height' => $this->meta['height'],
 		);
 	}
-	
+
 	/**
 	 * Creates a new, empty image with the desired size
 	 *
@@ -113,17 +115,17 @@ class Image {
 	private function create($x = null, $y = null, $ext = null){
 		if(!$x) $x = $this->meta['width'];
 		if(!$y) $y = $this->meta['height'];
-		
+
 		$image = imagecreatetruecolor($x, $y);
 		if(!$ext) $ext = $this->meta['ext'];
 		if($ext=='png'){
 			imagealphablending($image, false);
 			imagefilledrectangle($image, 0, 0, $x, $y, imagecolorallocatealpha($image, 0, 0, 0, 127));
 		}
-		
+
 		return $image;
 	}
-	
+
 	/**
 	 * Replaces the image resource with the given parameter
 	 *
@@ -132,11 +134,11 @@ class Image {
 	private function set($new){
 	  if(!empty($this->image)) imagedestroy($this->image);
 		$this->image = $new;
-		
+
 		$this->meta['width'] = imagesx($this->image);
 		$this->meta['height'] = imagesy($this->image);
 	}
-	
+
 	/**
 	 * Returns the path to the image file
 	 *
@@ -154,7 +156,7 @@ class Image {
 	public function getResource(){
 		return $this->image;
 	}
-	
+
 	/**
 	 * Rotates the image by the given angle
 	 *
@@ -164,12 +166,12 @@ class Image {
 	 */
 	public function rotate($angle, $bgcolor = null){
 		if(empty($this->image) || !$angle || $angle>=360) return $this;
-		
+
 		$this->set(imagerotate($this->image, $angle, is_array($bgcolor) ? imagecolorallocatealpha($this->image, $bgcolor[0], $bgcolor[1], $bgcolor[2], !empty($bgcolor[3]) ? $bgcolor[3] : null) : $bgcolor));
 
 		return $this;
 	}
-	
+
 	/**
 	 * Resizes the image to the given size, automatically calculates
 	 * the new ratio if parameter {@link $ratio} is set to true
@@ -184,54 +186,54 @@ class Image {
 	 */
 	public function resize($x = null, $y = null, $ratio = true, $resizeWhenSmaller = true){
 		if(empty($this->image) || (empty($x) && empty($y))) return false;
-		
+
 		$xStart = $x;
-    $yStart = $y;
-    $ratioX = $this->meta['width'] / $this->meta['height'];
-    $ratioY = $this->meta['height'] / $this->meta['width'];
-    //echo 'ALLOWED: <br>'.$xStart.'x'."<br>".$yStart.'y'."<br>---------------<br>"; 
-    // ->> keep the RATIO
-    if($ratio) {
-      //echo 'BEGINN: <br>'.$this->meta['width'].'x'."<br>".$this->meta['height'].'y'."<br><br>"; 
-        // -> align to WIDTH
-        if(!empty($x) && ($x < $this->meta['width'] || $resizeWhenSmaller))
-          $y = $x / $ratioX;
-        // -> align to HEIGHT
-        elseif(!empty($y) && ($y < $this->meta['height'] || $resizeWhenSmaller))
-          $x = $y / $ratioY;
-        else {
-          $y = $this->meta['height'];
-          $x = $this->meta['width'];
-        }        
-      //echo 'BET: <br>'.$x.'x'."<br>".$y.'y'."<br><br>";
-      // ->> align to WIDTH AND HEIGHT     
-      if((!empty($yStart) && $y > $yStart) || (!empty($xStart) && $x > $xStart)) {
-        if($y > $yStart) {
-          $y = $yStart;
-          $x = $y / $ratioY;
-        } elseif($x > $xStart) {
-          $x = $xStart;
-          $y = $x / $ratioX;
-        }
-      }
-    // ->> DONT keep the RATIO (but keep ration when only width OR height is set)
-    } else {
-      // RATIO X
-      if(!empty($y) && empty($x) && ($y < $this->meta['height'] || $resizeWhenSmaller))
-        $x = $y / $ratioX;
-      // RATIO Y
-      elseif(empty($y) && !empty($x) && ($x < $this->meta['width'] || $resizeWhenSmaller))
-        $y = $x / $ratioY;
-    }
-    $x = round($x);
-    $y = round($y);
-    
+	$yStart = $y;
+	$ratioX = $this->meta['width'] / $this->meta['height'];
+	$ratioY = $this->meta['height'] / $this->meta['width'];
+	//echo 'ALLOWED: <br>'.$xStart.'x'."<br>".$yStart.'y'."<br>---------------<br>";
+	// ->> keep the RATIO
+	if($ratio) {
+	  //echo 'BEGINN: <br>'.$this->meta['width'].'x'."<br>".$this->meta['height'].'y'."<br><br>";
+		// -> align to WIDTH
+		if(!empty($x) && ($x < $this->meta['width'] || $resizeWhenSmaller))
+		  $y = $x / $ratioX;
+		// -> align to HEIGHT
+		elseif(!empty($y) && ($y < $this->meta['height'] || $resizeWhenSmaller))
+		  $x = $y / $ratioY;
+		else {
+		  $y = $this->meta['height'];
+		  $x = $this->meta['width'];
+		}
+	  //echo 'BET: <br>'.$x.'x'."<br>".$y.'y'."<br><br>";
+	  // ->> align to WIDTH AND HEIGHT
+	  if((!empty($yStart) && $y > $yStart) || (!empty($xStart) && $x > $xStart)) {
+		if($y > $yStart) {
+		  $y = $yStart;
+		  $x = $y / $ratioY;
+		} elseif($x > $xStart) {
+		  $x = $xStart;
+		  $y = $x / $ratioX;
+		}
+	  }
+	// ->> DONT keep the RATIO (but keep ration when only width OR height is set)
+	} else {
+	  // RATIO X
+	  if(!empty($y) && empty($x) && ($y < $this->meta['height'] || $resizeWhenSmaller))
+		$x = $y / $ratioX;
+	  // RATIO Y
+	  elseif(empty($y) && !empty($x) && ($x < $this->meta['width'] || $resizeWhenSmaller))
+		$y = $x / $ratioY;
+	}
+	$x = round($x);
+	$y = round($y);
+
 		//echo 'END: <br>'.$x.'x'."<br>".$y.'y'."<br><br>";
-		
-		$new = $this->create($x, $y);
-		if(imagecopyresampled($new, $this->image, 0, 0, 0, 0, $x, $y, $this->meta['width'], $this->meta['height'])) {
-  		$this->set($new);
-  		return $this;
+
+			$new = $this->create($x, $y);
+			if(imagecopyresampled($new, $this->image, 0, 0, 0, 0, $x, $y, $this->meta['width'], $this->meta['height'])) {
+				$this->set($new);
+			return $this;
   	} else
   	 return false;
 	}
@@ -255,30 +257,30 @@ class Image {
 	 */
 	public function crop($top, $right = null, $bottom = null, $left = null){
 		if(empty($this->image)) return $this;
-		
+
 		if(!is_numeric($right) && !is_numeric($bottom) && !is_numeric($left))
 			$right = $bottom = $left = $top;
-		
+
 		if(!is_numeric($bottom) && !is_numeric($left)){
 			$bottom = $top;
 			$left = $right;
 		}
-		
+
 		if(!is_numeric($left))
 			$left = $right;
-		
+
 		$x = $this->meta['width']-$left-$right;
 		$y = $this->meta['height']-$top-$bottom;
 
 		if($x<0 || $y<0) return $this;
-		
+
 		$new = $this->create($x, $y);
 		imagecopy($new, $this->image, 0, 0, $left, $top, $x, $y);
 		$this->set($new);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Flips the image horizontally or vertically. To Flip both just use ->rotate(180)
 	 *
@@ -288,9 +290,9 @@ class Image {
 	 */
 	public function flip($type){
 		if(empty($this->image) || !in_array($type, array('horizontal', 'vertical'))) return $this;
-		
+
 		$new = $this->create();
-		
+
 		if($type=='horizontal')
 			for($x=0;$x<$this->meta['width'];$x++)
 				imagecopy($new, $this->image, $this->meta['width']-$x-1, 0, $x, 0, 1, $this->meta['height']);
@@ -299,10 +301,10 @@ class Image {
 				imagecopy($new, $this->image, 0, $this->meta['height']-$y-1, 0, $y, $this->meta['width'], 1);
 		
 		$this->set($new);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Stores the image in the desired directory or overwrite the old one
 	 *
@@ -311,21 +313,21 @@ class Image {
 	 */
 	public function process($ext = null, $file = null){
 	  if(!empty($this->image)) {
-  		if(!$ext) $ext = $this->meta['ext'];
+		if(!$ext) $ext = $this->meta['ext'];
   		if($ext=='jpg')	$ext = 'jpeg';
-      if($ext=='png') imagesavealpha($this->image, true);
-  		
-  		if($file == null)
-  		  $file = $this->file;
-  		
-      $fn = 'image'.$ext;
-  		if($ext == 'jpeg')
+		if($ext=='png') imagesavealpha($this->image, true);
+
+		if($file == null)
+		  $file = $this->file;
+
+		$fn = 'image'.$ext;
+		if($ext == 'jpeg')
   		  $fn($this->image, $file,100);
-  		else
+		else
   		  $fn($this->image, $file);
   		  
-  		
-  		// If there is a new filename change the internal name too
+
+		// If there is a new filename change the internal name too
   		if($file) $this->file = $file;
   		return true;
 		} else
@@ -340,20 +342,20 @@ class Image {
 	 */
 	public function save($file = null){
 		if(empty($this->image)) return $this;
-		
+
 		if(!$file) $file = $this->file;
-		
+
 		$ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 		if(!$ext){
 			$file .= '.'.$this->meta['ext'];
 			$ext = $this->meta['ext'];
 		}
-		
+
 		if($ext=='jpg') $ext = 'jpeg';
-		
+
 		if(!in_array($ext, array('png', 'jpeg', 'gif')))
 			return $this;
-		
+
 		$this->process($ext, $file);
 		
 		return $this;
@@ -366,11 +368,10 @@ class Image {
 	 */
 	public function show(){
 		if(empty($this->image)) return $this;
-		
+
 		header('Content-type: '.$this->meta['mime']);
 		$this->process();
 		
 		return $this;
 	}
-	
 }
