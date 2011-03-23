bc4923d1df0c87237ced64c96443cd5fc604ba7d
Ger Hobbelt <ger@hobbelt.com>
2011-03-23 20:51:10 +0100

    Gallery: make sure the green [+] icon is gone when the drag&drop action is finished.

--- Source/Gallery.js
+++ Source/Gallery.js
@@ -132,6 +132,9 @@ FileManager.Gallery = new Class({
   },
 
   onDragComplete: function(el, droppable) {
+
+	this.imageadd.fade(0);
+
     if(this.howto){
       this.howto.destroy();
       this.howto = null;
