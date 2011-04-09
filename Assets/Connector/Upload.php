232a5e116ab85aba3d54ec5311fd5b761e31a69e
Naki <naki@pvpgn.pl>
2011-04-09 20:47:33 +0200

    Added missing 'static' declaration in methods.

--- Assets/Connector/Upload.php
+++ Assets/Connector/Upload.php
@@ -76,7 +76,7 @@ class Upload {
 	 * @param string $file
 	 * @return bool
 	 */
-	public function exists($file){
+	public static function exists($file){
 		return !(empty($_FILES[$file]['name']) || empty($_FILES[$file]['size']));
 	}
 	
@@ -86,7 +86,7 @@ class Upload {
 	 * @param string $file
 	 * @param array $options
 	 */
-	public function mime($file, $options = array()){
+	public static function mime($file, $options = array()){
 		$file = realpath($file);
 		$options = array_merge(array(
 			'default' => null,
