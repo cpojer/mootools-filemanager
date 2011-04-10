f0688fad075b8ded8ac39742ea77d80485bc17e4
Ger Hobbelt <ger@hobbelt.com>
2011-04-10 14:10:37 +0200

    whitespace and moving/renaming gallery show() to show_gallery() to make it clearly different from FileManager.show() while moving it into class scope. Prep work for the next commit.

--- Source/Gallery.js
+++ Source/Gallery.js
@@ -20,34 +20,11 @@ FileManager.Gallery = new Class({
 
 	Extends: FileManager,
 
-	initialize: function(options) {
+	initialize: function(options)
+	{
 		this.offsets = {y: -72};
 		//this.galleryPlugin = true; // prevent that this.show() is called in the base class again
 		this.parent(options);
-
-		var show = function() {
-			this.galleryContainer.setStyles({
-				opacity: 0,
-				display: 'block'
-			});
-
-			this.filemanager.setStyles({
-				top: '10%',
-				height: '60%'
-			});
-			this.fitSizes();
-
-			var size = this.filemanager.getSize();
-			var pos = this.filemanager.getPosition();
-			this.galleryContainer.setStyles({
-				top: pos.y + size.y - 1,
-				left: pos.x + (size.x - this.galleryContainer.getWidth()) / 2,
-				opacity: 1
-			});
-
-			this.hideClone();
-			this.wrapper.setStyle('display', 'none');
-		};
 
 		this.addEvents({
 			scroll: show,
@@ -57,7 +34,7 @@ FileManager.Gallery = new Class({
 			}),
 
 			hide: function(){
-				if(!this.keepData) {
+				if (!this.keepData) {
 					this.gallery.empty();
 					this.captions = {};
 					this.files = [];
@@ -70,11 +47,11 @@ FileManager.Gallery = new Class({
 			},
 
 			modify: function(file){
-				var name = this.normalize(file.path);
-				var el = (this.gallery.getElements('li').filter(function(el){
-					var f = el.retrieve('file');
-					return name == this.normalize(f.path);
-				}, this) || null)[0];
+					var name = this.normalize(file.path);
+					var el = (this.gallery.getElements('li').filter(function(el){
+						var f = el.retrieve('file');
+						return name == this.normalize(f.path);
+					}, this) || null)[0];
 
 				if (el) this.erasePicture(name, el);
 			}
@@ -84,11 +61,13 @@ FileManager.Gallery = new Class({
 		this.galleryContainer = new Element('div', {'class': 'filemanager-gallery'}).inject(this.container);
 		this.gallery = new Element('ul').inject(this.galleryContainer);
 
-		var timer, removeClone = this.removeClone.bind(this);
+		var timer;
+		var removeClone = this.removeClone.bind(this);
 
 		this.input = new Element('input', {name: 'imgCaption'}).addEvent('keyup', function(e){
-			if (e.key == 'enter')
+			if (e.key == 'enter') {
 				removeClone(e);
+			}
 		});
 		this.wrapper = new Element('div', {
 			'class': 'filemanager-wrapper',
@@ -128,6 +107,31 @@ FileManager.Gallery = new Class({
 	initialShow: function() {
 	},
 
+	show_gallery: function()
+	{
+		this.galleryContainer.setStyles({
+			opacity: 0,
+			display: 'block'
+		});
+
+		this.filemanager.setStyles({
+			top: '10%',
+			height: '60%'
+		});
+		this.fitSizes();
+
+		var size = this.filemanager.getSize();
+		var pos = this.filemanager.getPosition();
+		this.galleryContainer.setStyles({
+			top: pos.y + size.y - 1,
+			left: pos.x + (size.x - this.galleryContainer.getWidth()) / 2,
+			opacity: 1
+		});
+
+		this.hideClone();
+		this.wrapper.setStyle('display', 'none');
+	},
+
 	// override the parent's allow_DnD method: always allow drag and drop as otherwise we cannot construct our gallery!
 	allow_DnD: function(j, pagesize) {
 		return true;
@@ -137,12 +141,13 @@ FileManager.Gallery = new Class({
 
 		this.imageadd.fade(0);
 
-		if(this.howto){
+		if (this.howto){
 			this.howto.destroy();
 			this.howto = null;
 		}
 
-		if (!droppable || droppable != this.gallery) return false;
+		if (!droppable || droppable != this.gallery)
+			return false;
 
 		var file;
 		if (typeOf(el) == 'string'){
@@ -165,7 +170,9 @@ FileManager.Gallery = new Class({
 		var destroyIcon = new Asset.image(this.assetBasePath + 'Images/destroy.png').set({
 			'class': 'filemanager-remove',
 			title: this.language.gallery.remove,
-			events: {click: this.removePicture}
+			events: {
+				click: this.removePicture
+			}
 		}).store('gallery', this);
 
 		var li = new Element('li').store('file', file).adopt(
@@ -237,11 +244,14 @@ FileManager.Gallery = new Class({
 	},
 
 	removeClone: function(e){
-		if (!this.clone || (e.relatedTarget && ([this.clone, this.wrapper].contains(e.relatedTarget) || (this.wrapper.contains(e.relatedTarget) && e.relatedTarget != this.wrapper)))) return;
-		if (this.clone.get('morph').timer) return;
+		if (!this.clone || (e.relatedTarget && ([this.clone, this.wrapper].contains(e.relatedTarget) || (this.wrapper.contains(e.relatedTarget) && e.relatedTarget != this.wrapper))))
+			return;
+		if (this.clone.get('morph').timer)
+			return;
 
 		var file = this.clone.retrieve('file');
-		if (!file) return;
+		if (!file)
+			return;
 
 		this.captions[this.normalize(file.dir + '/' + file.name)] = (this.input.get('value') || '');
 
@@ -256,7 +266,8 @@ FileManager.Gallery = new Class({
 	},
 
 	hideClone: function(){
-		if (!this.clone) return;
+		if (!this.clone)
+			return;
 
 		this.clone.get('morph').cancel();
 		var parent = this.clone.retrieve('parent');
@@ -269,7 +280,7 @@ FileManager.Gallery = new Class({
 	},
 
 	removePicture: function(e){
-		if(e) e.stop();
+		if (e) e.stop();
 
 		var self = this.retrieve('gallery'),
 			parent = this.getParent('li'),
@@ -292,13 +303,14 @@ FileManager.Gallery = new Class({
 	},
 
 	switchButton: function(){
-		if(typeof this.gallery != 'undefined') {
+		if (typeof this.gallery != 'undefined') {
 			var chk = !!this.gallery.getChildren().length;
 			this.menu.getElement('button.filemanager-serialize').set('disabled', !chk)[(chk ? 'remove' : 'add') + 'Class']('disabled');
 		}
 	},
 
-	populate: function(data){
+	populate: function(data)
+	{
 		Array.each(data || {}, function(v, i){
 			this.captions[i] = v;
 			this.onDragComplete(i, this.gallery);
@@ -306,7 +318,7 @@ FileManager.Gallery = new Class({
 	},
 
 	serialize_on_click: function(e){
-		if(e) e.stop();
+		if (e) e.stop();
 		var serialized = {};
 		this.files.each(function(v){
 			serialized[v] = (this.captions[v] || '');
