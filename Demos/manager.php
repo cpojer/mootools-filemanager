096c89cea594482b36f80c0e8dec264bae8f1b9b
Christoph Pojer <christoph.pojer@gmail.com>
2011-04-18 11:09:08 +0200

    Removing the "security measures" in the demos. Those are demos after all.

--- Demos/manager.php
+++ Demos/manager.php
@@ -1,5 +1,4 @@
 <?php
-die(json_encode(array('status'=> 0, 'error' => "\nSecurity precaution... to enable the demo, edit\nDemos/".basename(__FILE__)."\nand comment out line 2.")));
 
 error_reporting(E_ALL | E_STRICT);
 
